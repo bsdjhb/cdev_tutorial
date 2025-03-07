03f9ac8f658373c869f0e7443819d3e4e570bca9
John Baldwin <jhb@FreeBSD.org>
2025-03-07 15:35:29 -0500

    mappage: Use a physical memory VM object with the mmap_single method
    The single page of wired memory is allocated on the first fault by the
    physical pager.  Since the physical memory pager frees its pages on
    destruction, the driver can be safely unloaded.  The wired memory will
    be freed once the last mapping is deleted after the driver has been
    unloaded.

--- mappage/mappage.c
+++ mappage/mappage.c
@@ -7,62 +7,83 @@
 #include <sys/param.h>
 #include <sys/conf.h>
 #include <sys/kernel.h>
-#include <sys/malloc.h>
 #include <sys/module.h>
+#include <sys/rwlock.h>
 #include <vm/vm.h>
-#include <vm/pmap.h>
+#include <vm/vm_object.h>
+#include <vm/vm_page.h>
+#include <vm/vm_pager.h>
 
-static MALLOC_DEFINE(M_MAPPAGE, "mappage", "Demo mappage character device");
-
-static d_mmap_t mappage_mmap;
+static d_mmap_single_t mappage_mmap_single;
 
 static struct cdevsw mappage_cdevsw = {
 	.d_version =	D_VERSION,
-	.d_mmap =	mappage_mmap,
+	.d_mmap_single = mappage_mmap_single,
 	.d_name =	"mappage"
 };
 
 static int
-mappage_mmap(struct cdev *dev, vm_ooffset_t offset, vm_paddr_t *paddr,
-    int nprot, vm_memattr_t *memattr)
+mappage_mmap_single(struct cdev *cdev, vm_ooffset_t *offset, vm_size_t size,
+    struct vm_object **object, int nprot)
 {
-	if (offset != 0)
+	vm_object_t obj;
+
+	obj = cdev->si_drv1;
+	if (OFF_TO_IDX(round_page(*offset + size)) > obj->size)
 		return (EINVAL);
 
-	*paddr = pmap_kextract((uintptr_t)dev->si_drv1);
+	vm_object_reference(obj);
+	*object = obj;
 	return (0);
 }
 
 static int
-mappage_create(void)
+mappage_create(struct cdev **cdevp)
 {
 	struct make_dev_args args;
-	struct cdev *cdev;
-	void *page;
+	vm_object_t obj;
 	int error;
 
-	page = malloc(PAGE_SIZE, M_MAPPAGE, M_WAITOK | M_ZERO);
+	obj = vm_pager_allocate(OBJT_PHYS, NULL, PAGE_SIZE,
+	    VM_PROT_DEFAULT, 0, NULL);
+	if (obj == NULL)
+		return (ENOMEM);
 	make_dev_args_init(&args);
 	args.mda_flags = MAKEDEV_WAITOK | MAKEDEV_CHECKNAME;
 	args.mda_devsw = &mappage_cdevsw;
 	args.mda_uid = UID_ROOT;
 	args.mda_gid = GID_WHEEL;
 	args.mda_mode = 0600;
-	args.mda_si_drv1 = page;
-	error = make_dev_s(&args, &cdev, "mappage");
+	args.mda_si_drv1 = obj;
+	error = make_dev_s(&args, cdevp, "mappage");
 	if (error != 0) {
-		free(page, M_MAPPAGE);
+		vm_object_deallocate(obj);
 		return (error);
 	}
 	return (0);
 }
 
+static void
+mappage_destroy(struct cdev *cdev)
+{
+	if (cdev == NULL)
+		return;
+
+	vm_object_deallocate(cdev->si_drv1);
+	destroy_dev(cdev);
+}
+
 static int
 mappage_modevent(module_t mod, int type, void *data)
 {
+	static struct cdev *mappage_cdev;
+
 	switch (type) {
 	case MOD_LOAD:
-		return (mappage_create());
+		return (mappage_create(&mappage_cdev));
+	case MOD_UNLOAD:
+		mappage_destroy(mappage_cdev);
+		return (0);
 	default:
 		return (EOPNOTSUPP);
 	}
